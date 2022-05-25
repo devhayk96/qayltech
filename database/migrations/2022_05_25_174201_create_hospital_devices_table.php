5ce81c3c278502d51832289ed0f17f37f6394fff
margaryan <devhayk1996@gmail.com>
2022-05-25 21:59:01 +0400

    created hospital devices table

--- database/migrations/2022_05_25_174201_create_hospital_devices_table.php
+++ database/migrations/2022_05_25_174201_create_hospital_devices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCountriesTable extends Migration
+class CreateHospitalDevicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateCountriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create('hospital_devices', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('hospital_id')->constrained('hospitals');
+            $table->foreignId('device_id')->constrained('devices');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateCountriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('countries');
+        Schema::dropIfExists('hospital_devices');
     }
 }
