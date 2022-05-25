1e77a6e4aeec06f9795129884519c76da1f8a6a3
grigor <marukhyan.grig@mail.ru>
2022-05-25 22:04:10 +0400

    add realtions

---
+++ app/Models/Hospital.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hospital extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'address',
+        'category_id',
+        'organization_id'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->hasOne(Category::class);
+    }
+
+    public function organization(){
+        return $this->belongsTo(Organization::class);
+    }
+}
---
+++ app/Models/Patient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'birth_date',
+        'disability_date',
+        'disability_reason',
+        'disability_category',
+        'injury',
+        'workout_begin',
+        'is_individual',
+        'image',
+        'pdf'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
