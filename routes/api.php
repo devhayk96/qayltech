11fa7dc5661202afdfdf1d86c65cd316a30e2af3
grigor <marukhyan.grig@mail.ru>
2022-05-26 21:36:54 +0400

    create controllers and routes for models

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\CountriesController;
+use App\Http\Controllers\DevicesController;
+use App\Http\Controllers\DoctorsController;
+use App\Http\Controllers\HospitalsController;
+use App\Http\Controllers\OrganizationsController;
+use App\Http\Controllers\PatientsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -24,5 +31,12 @@ Route::middleware('auth:api')->group(function () {
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::post('login', [AuthController::class, 'login']);
+    Route::resource('categories', [CategoriesController::class]);
+    Route::resource('countries', [CountriesController::class]);
+    Route::resource('devices', [DevicesController::class]);
+    Route::resource('doctors', [DoctorsController::class]);
+    Route::resource('hospitals', [HospitalsController::class]);
+    Route::resource('organizations', [OrganizationsController::class]);
+    Route::resource('patients', [PatientsController::class]);
 //    Route::post('register','Auth\ApiAuthController@register');
 });
