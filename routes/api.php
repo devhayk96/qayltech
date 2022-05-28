fc9fe102c1f5eef456aecf5967e6bdc8734d9bf0
devhayk <devhayk1996@gmail.com>
2022-05-28 17:17:31 +0400

    did fixes in helper and route files

--- routes/api.php
+++ routes/api.php
@@ -21,22 +21,22 @@ use App\Http\Controllers\Api\AuthController;
 |
 */
 
-Route::middleware('auth:api')->group(function () {
+Route::group(['middleware' => ['cors', 'json.response', 'auth:api']], function () {
     Route::get('user', function (Request $request) {
         return $request->user();
     });
 
-    Route::resource('categories', CategoriesController::class);
-    Route::resource('countries', CountriesController::class);
-    Route::resource('devices', DevicesController::class);
-    Route::resource('doctors', DoctorsController::class);
-    Route::resource('hospitals', HospitalsController::class);
-    Route::resource('organizations', OrganizationsController::class);
-    Route::resource('patients', PatientsController::class);
+    Route::apiResource('categories', CategoriesController::class);
+    Route::apiResource('countries', CountriesController::class);
+    Route::apiResource('devices', DevicesController::class);
+    Route::apiResource('doctors', DoctorsController::class);
+    Route::apiResource('hospitals', HospitalsController::class);
+    Route::apiResource('organizations', OrganizationsController::class);
+    Route::apiResource('patients', PatientsController::class);
 
     Route::post('logout', [AuthController::class, 'logout']);
 });
-Route::resource('countries', CountriesController::class);
+
 Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::post('login', [AuthController::class, 'login']);
 //    Route::post('register','Auth\ApiAuthController@register');
