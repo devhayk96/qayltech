d74a8d4d361528df7bf07f0be9ac3236299d30d2
devhayk <devhayk1996@gmail.com>
2022-06-23 18:52:50 +0400

    fixed saving user role issue and category unique

--- app/Http/Requests/Category/StoreRequest.php
+++ app/Http/Requests/Category/StoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Category;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreRequest extends FormRequest
 {
@@ -13,9 +14,19 @@ class StoreRequest extends FormRequest
      */
     public function rules()
     {
+        $name = request()->get('name');
+        $type = request()->get('type');
+
         return [
             'name' => 'required|string|max:255',
-            'type' => 'required|string|max:255',
+            'type' => ['required', 'string', 'max:255', Rule::unique('categories')
+                ->where(function ($query) use($name, $type) {
+                    return $query->where([
+                        'name' => $name,
+                        'type' => $type
+                    ]);
+                }),
+            ],
         ];
     }
 }
